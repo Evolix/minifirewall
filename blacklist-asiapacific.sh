40ea8c4cfdcbdf571d73d8b6a26323e9d0fbe25d
Gregory Colpart <gcolpart+git@evolix.fr>
2024-11-09 18:15:41 +0100

    on passe à ipset

--- blacklist-asiapacific.sh
+++ blacklist-asiapacific.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# use it with /sbin/iptables -I INPUT -m set --match-set apnic-ipv4 src -j DROP
+
 cd /var/tmp
 
 rm -f $apnicdeny_file
@@ -7,9 +9,10 @@ rm -f $apnicdeny_file
 GET http://antispam00.evolix.org/spam/apnic.cidr.md5 > apnic.cidr.md5
 GET http://antispam00.evolix.org/spam/apnic.cidr > apnic.cidr
 
-/sbin/iptables -F NEEDRESTRICT
+ipset destroy apnic-ipv4
+ipset create apnic-ipv4 hash:net
 
 for i in $(cat /var/tmp/apnic.cidr); do
     BLOCK=$(echo $i | cut -d"|" -f2)
-    /sbin/iptables -I NEEDRESTRICT -s $BLOCK -j DROP
+    /sbin/ipset add apnic-ipv4 $BLOCK
 done
